d0f998b94939c89769d454f4f1327da013a81dec
doldboy <384789980@qq.com>
2021-11-29 16:31:13 +0800

    封装统一的请求返回格式

--- controller/UserController.go
+++ controller/UserController.go
@@ -3,10 +3,12 @@ package controller
 import (
 	"fmt"
 	"go-gin-vue/common"
+	"go-gin-vue/dto"
 	"go-gin-vue/model"
+	"go-gin-vue/response"
 	"go-gin-vue/util"
-	"net/http"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
@@ -23,11 +25,13 @@ func Register(ctx *gin.Context) {
 
 	// 数据验证
 	if len(telephone) != 11 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "手机号必须为11位")
+		// ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
 		return
 	}
 	if len(password) < 6 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "密码不能少于6位")
+		// ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
 		return
 	}
 	// 如果名字没有传参，给一个10位的随机字符串
@@ -39,14 +43,16 @@ func Register(ctx *gin.Context) {
 
 	// 判断手机号是否存在
 	if isTelephoneExist(DB, telephone) {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已经存在，不能注册"})
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "用户已经存在，不能注册")
+		// ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已经存在，不能注册"})
 		return
 	}
 
 	// 创建用户
 	hasePassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "加密错误"})
+		response.Response(ctx, http.StatusInternalServerError, 500, nil, "加密错误")
+		// ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "加密错误"})
 		return
 	}
 
@@ -58,7 +64,8 @@ func Register(ctx *gin.Context) {
 	DB.Create(&newUser)
 
 	// 返回JSON格式的数据
-	ctx.JSON(200, gin.H{"code": 200, "msg": "注册成功"})
+	response.Success(ctx, nil, "注册成功")
+	// ctx.JSON(200, gin.H{"code": 200, "msg": "注册成功"})
 }
 
 func Login(ctx *gin.Context) {
@@ -69,11 +76,13 @@ func Login(ctx *gin.Context) {
 
 	// 数据验证
 	if len(telephone) != 11 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "手机号必须为11位")
+		// ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
 		return
 	}
 	if len(password) < 6 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "密码不能少于6位")
+		// ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
 		return
 	}
 
@@ -81,7 +90,8 @@ func Login(ctx *gin.Context) {
 	var user model.User
 	DB.Where("telephone = ?", telephone).First(&user)
 	if (user.ID == 0) {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户不存在"})
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "用户不存在")
+		// ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户不存在"})
 		return
 	}
 
@@ -89,21 +99,24 @@ func Login(ctx *gin.Context) {
 	// 发放 token 给前端
 	token, err := common.ReleaseToken(user)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "系统异常"})
+		response.Response(ctx, http.StatusInternalServerError, 500, nil, "系统异常")
+		// ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "系统异常"})
 		log.Printf("token generate err : %v", err)
 		return
 	}
 
-	ctx.JSON(200, gin.H{
-		"code": 200,
-		"data": gin.H{"token": token},
-		"msg": "登陆成功",
-	})
+	// 成功返回结果
+	// ctx.JSON(200, gin.H{
+	// 	"code": 200,
+	// 	"data": gin.H{"token": token},
+	// 	"msg": "登陆成功",
+	// })
+	response.Success(ctx, gin.H{"token": token}, "登陆成功")
 }
 
 func Info(ctx *gin.Context) {
 	user, _ := ctx.Get("user")
-	ctx.JSON(http.StatusOK, gin.H{"code": 200, "data": gin.H{"user": user}})
+	ctx.JSON(http.StatusOK, gin.H{"code": 200, "data": gin.H{"user": dto.ToUserDto(user.(model.User))}})
 }
 
 func isTelephoneExist(db *gorm.DB, telephone string) bool {
