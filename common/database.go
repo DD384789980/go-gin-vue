c99783d8d38c6488477db014d99ec49e7e4781c9
doldboy <384789980@qq.com>
2021-12-01 17:45:36 +0800

    viper-从文件中读取配置

--- common/database.go
+++ common/database.go
@@ -1,23 +1,38 @@
 package common
 
 import (
+	"fmt"
 	"go-gin-vue/model"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/spf13/viper"
 )
 
 var DB *gorm.DB
 
-// 开启连接池
+
 func InitDB() *gorm.DB {
-	db, err := gorm.Open("mysql", "root:3K7HozUxGp@(192.168.10.231:3306)/gin_vue?charset=utf8mb4&parseTime=True&loc=Local")
+	driverName := viper.GetString("datasource.driverName")
+	host := viper.GetString("datasource.host")
+	port := viper.GetString("datasource.port")
+	database := viper.GetString("datasource.database")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	charset := viper.GetString("datasource.charset")
+	args := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=%s&parseTime=true",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset)
+	db, err := gorm.Open(driverName, args)
 	if err != nil {
 		panic("fail to connect database, err:" + err.Error())
 	}
 	
 	// gorm 自动创建数据表
 	db.AutoMigrate(&model.User{})
-
 	DB = db
 	return db
 }
--- main.go
+++ main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
-	"go-gin-vue/routers"
 	"go-gin-vue/common"
+	"go-gin-vue/routers"
+
+	"os"
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/spf13/viper"
 )
 
 
 
 func main() {
+	InitConfig()
 	db := common.InitDB()
 	defer db.Close()
 
 	r := gin.Default()
 	r = routers.CollectRoute(r)
 
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
+
 	//启动服务
-	panic(r.Run(":8082"))	
+	panic(r.Run(":8080"))	
+}
+
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+	}
 }
